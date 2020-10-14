5ba4c52da19b5b4e32ee2f84b291343fde9a58e1
Alex <alexander.reiprich@hs-furtwangen.de>
2020-10-14 18:30:30 +0200

    Finished L01

--- L01_FirstFudge/Main.js
+++ L01_FirstFudge/Main.js
@@ -1,6 +1,26 @@
 "use strict";
 var L01_FirstFudge;
 (function (L01_FirstFudge) {
-    console.log("Ja moin.");
+    var f = FudgeCore;
+    window.addEventListener("load", load);
+    function load(_event) {
+        const canvas = document.querySelector("canvas");
+        //f.Debug.log(canvas);
+        let node = new f.Node("Quad");
+        let mesh = new f.MeshQuad;
+        let componentMesh = new f.ComponentMesh(mesh);
+        node.addComponent(componentMesh);
+        let materialSolidWhite = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("WHITE")));
+        let componentMaterial = new f.ComponentMaterial(materialSolidWhite);
+        node.addComponent(componentMaterial);
+        let componentCamera = new f.ComponentCamera();
+        componentCamera.pivot.translateZ(3);
+        componentCamera.pivot.rotateY(180);
+        componentCamera.pivot.rotateZ(45);
+        L01_FirstFudge.viewport = new f.Viewport();
+        L01_FirstFudge.viewport.initialize("Viewport", node, componentCamera, canvas);
+        //f.Debug.log(canvas);
+        L01_FirstFudge.viewport.draw();
+    }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
 //# sourceMappingURL=Main.js.map
--- L01_FirstFudge/Main.ts
+++ L01_FirstFudge/Main.ts
@@ -1,7 +1,33 @@
 namespace L01_FirstFudge {
 
-    console.log("Ja moin.");
+    import f = FudgeCore;
+    window.addEventListener("load", load);
+    export let viewport: f.Viewport;
 
+    function load(_event: Event): void {
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+        //f.Debug.log(canvas);
+        let node: f.Node = new f.Node("Quad");
+
+        let mesh: f.MeshQuad = new f.MeshQuad;
+        let componentMesh: f.ComponentMesh = new f.ComponentMesh(mesh);
+        node.addComponent(componentMesh);
+
+        let materialSolidWhite: f.Material = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(f.Color.CSS("WHITE")));
+        let componentMaterial: f.ComponentMaterial = new f.ComponentMaterial(materialSolidWhite);
+        node.addComponent(componentMaterial);
+
+        let componentCamera: f.ComponentCamera = new f.ComponentCamera();
+        componentCamera.pivot.translateZ(3);
+        componentCamera.pivot.rotateY(180);
+        componentCamera.pivot.rotateZ(45);
+
+        viewport = new f.Viewport();
+        viewport.initialize("Viewport", node, componentCamera, canvas);
+        
+        //f.Debug.log(canvas);
+
+        viewport.draw();
+    }
     
-
 }
